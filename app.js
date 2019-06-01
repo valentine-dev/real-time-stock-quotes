ec3499801150263a701c80617c622642c502d3a9
Valentine Wu <valentine.business@gmail.com>
2019-06-01 16:24:37 -0700

    add configuration file & correct package.json

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const port = 3000;
 const axios = require('axios');
+const express = require('express');
+const config = require('config');
 
-const host = 'https://app.quotemedia.com/datatool/getSnapQuotes.json';
+// port
+const configuredPort = config.has('server.port') ? config.get('server.port') : 3000;
+const envPort = parseInt(process.env.NODE_SERVER_PORT, 10);
+const port = isNaN(envPort) ? configuredPort : envPort;
+console.log("----> Port to listen to: " + port);
 
-app.get('/test', (req, res) => res.send('App is running well? Yes!'));
-app.get('/', (req, res) => res.send('Usage: /quote?symbol=xxx&token=xxx'));
+// host
+const host = config.has('access.url') ? config.get('access.url') : null;
+if (!host) {
+   console.error("No access URL in the configuration file.");
+   return;
+} else {
+   console.log("----> Access URL: " + host);
+}
 
+// host 
+const headers = config.has('access.headers') ? config.get('access.headers') : { 'Origin': 'https://web.tmxmoney.com' };
+console.log("----> Access Headers: " + JSON.stringify(headers));
+
+// help messasges
+const app = express();
+const testMessage = config.has('help.message.test') ? config.get('help.message.test') : 'Fine';
+const defaultMessage = config.has('help.message.default') ? config.get('help.message.default') : 'Usage: /quote?symbol=xxx&token=xxx';
+
+app.get('/test', (req, res) => res.send(testMessage));
+app.get('/', (req, res) => res.send(defaultMessage));
 app.get('/quote', (req, res) => {
    const symbol = req.query.symbol;
    const token = req.query.token;
@@ -17,24 +37,23 @@ app.get('/quote', (req, res) => {
       axios({
          method: 'get',
          url: host + '?symbols=' + symbol + '&token=' + token,
-         headers: { 'Origin': 'https://web.tmxmoney.com' }
-      })
-         .then(resp => {
-            const quote = resp.data.results.quote[0];
-            console.log("Got quote info ... ");
-            console.log(quote);
-            const exchange = quote.key.exchange
-            if (exchange) {
-               res.status(200).send(quote);
-            } else {
-               const noSuchSymbol = "Cannot find any exchanges with entered quote - " + symbol;
-               console.log(noSuchSymbol);
-               res.status(400).send(noSuchSymbol);
-            }
-         }).catch(error => {
-            console.error(error);
-            res.send(error);
-         });
+         headers: headers
+      }).then(resp => {
+         const quote = resp.data.results.quote[0];
+         console.log("Got quote info ... ");
+         console.log(quote);
+         const exchange = quote.key.exchange
+         if (exchange) {
+            res.status(200).send(quote);
+         } else {
+            const noSuchSymbol = "Cannot find any exchanges with entered quote - " + symbol;
+            console.log(noSuchSymbol);
+            res.status(400).send(noSuchSymbol);
+         }
+      }).catch(error => {
+         console.error(error);
+         res.send(error);
+      });
    } else {
       res.status(400).send('Bad Request. Usage: /quote?symbol=xxx&token=xxx');
    }
