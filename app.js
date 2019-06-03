1a18d7a125715f4ef7004211f16aaeefcccb19fc
Valentine Wu <valentine.business@gmail.com>
2019-06-03 15:11:12 -0700

    Remove config, improve ui

--- app.js
+++ app.js
@@ -2,48 +2,37 @@
 
 const axios = require('axios');
 const express = require('express');
-const config = require('config');
 const path = require('path');
 
 // port
-const configuredPort = config.has('server.port') ? config.get('server.port') : 3000;
-const envPort = parseInt(process.env.NODE_SERVER_PORT, 10);
-const port = isNaN(envPort) ? configuredPort : envPort;
+const port = 8080;
 console.log("----> Port to listen to: " + port);
 
 // host
-const host = config.has('access.url') ? config.get('access.url') : null;
-if (!host) {
-   console.error("No access URL in the configuration file.");
-   return;
-} else {
-   console.log("----> Access URL: " + host);
-}
+const host = 'https://app.quotemedia.com/datatool/getSnapQuotes.json';
+console.log("----> Access URL: " + host);
 
 // host 
-const headers = config.has('access.headers') ? config.get('access.headers') : { 'Origin': 'https://web.tmxmoney.com' };
+const headers = { 'Origin': 'https://web.tmxmoney.com' };
 console.log("----> Access Headers: " + JSON.stringify(headers));
-
-// response
-//const cors = config.has('response.headers.accessControlAllowOrigin') ? config.get('response.headers.accessControlAllowOrigin') : '*';
 
 // help messasges
 const app = express();
-const testMessage = config.has('help.message.test') ? config.get('help.message.test') : 'Fine';
-const defaultMessage = config.has('help.message.default') ? config.get('help.message.default') : 'Usage: /quote?symbol=xxx&token=xxx';
+const testMessage = 'Fine';
+const defaultMessage = 'API Usage: /quote?symbol=xxx&token=xxx';
 
-app.get('/test', (req, res) => res.send(testMessage));
-// app.get('/', (req, res) => res.send(defaultMessage));
-app.use(express.static(path.join(__dirname, 'build')));
+app.get('/test', (req, res) => res.send(testMessage).end());
+app.get('/', (req, res) => res.send(defaultMessage).end());
+app.use('/ui', express.static(path.join(__dirname, 'ui')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/ui', function(req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html')).end();
 });
 
 app.get('/quote', (req, res) => {
    const symbol = req.query.symbol;
    const token = req.query.token;
-   //res.set('Access-Control-Allow-Origin', cors);
+   
    if (symbol && token) {
       axios({
          method: 'get',
@@ -55,18 +44,18 @@ app.get('/quote', (req, res) => {
          console.log(quote);
          const exchange = quote.key.exchange
          if (exchange) {
-            res.status(200).send(quote);
+            res.status(200).send(quote).end();
          } else {
             const noSuchSymbol = "Cannot find any exchanges with entered symbol - " + symbol;
             console.log(noSuchSymbol);
-            res.status(400).send(noSuchSymbol);
+            res.status(400).send(noSuchSymbol).end();
          }
       }).catch(error => {
          console.error(error);
-         res.send(error);
+         res.send(error).end();
       });
    } else {
-      res.status(400).send('Bad Request. Usage: /quote?symbol=xxx&token=xxx');
+      res.status(400).send('Bad Request. Usage: /quote?symbol=xxx&token=xxx').end();
    }
 
 });
--- config/readme.md
+++
@@ -1,4 +0,0 @@
-# Config Node.js Applications
-
-https://github.com/lorenwest/node-config
-https://github.com/lorenwest/node-config/wiki/Configuration-Files
