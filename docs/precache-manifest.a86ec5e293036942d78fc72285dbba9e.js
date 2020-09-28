9584bfd03809df5e46ea465ac02391385d1eb75f
Valentine Wu <valentine.wu@calabrio.com>
2020-09-28 01:04:09 -0700

    Version 0.1.5. build

---
+++ docs/precache-manifest.a86ec5e293036942d78fc72285dbba9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb4d74d3d5630cb9be3ce264301b414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "461862388ece68355a65",
+    "url": "/static/js/2.271679d3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.271679d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1276317333c410d8b61",
+    "url": "/static/js/main.79d90e38.chunk.js"
+  },
+  {
+    "revision": "16e6e0a59cb8b86337ae",
+    "url": "/static/js/runtime-main.3bd5c55c.js"
+  }
+]);
