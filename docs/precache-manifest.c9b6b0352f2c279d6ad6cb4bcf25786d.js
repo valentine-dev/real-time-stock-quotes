22324f89c9a5818482e8245cad8ce7844d5ee9c5
valentine-dev <valentine.business@gmail.com>
2022-09-20 23:00:01 -0700

    Upload production build for release 0.3.1

--- docs/precache-manifest.c9b6b0352f2c279d6ad6cb4bcf25786d.js
+++ docs/precache-manifest.c9b6b0352f2c279d6ad6cb4bcf25786d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "278654f5ff57a10c1b1f8c99735de370",
+    "revision": "2d38993fa4902ff42f412a73de8914a4",
     "url": "./index.html"
   },
   {
-    "revision": "2cd2b8cddba1fc01a237",
-    "url": "./static/js/2.123e983e.chunk.js"
+    "revision": "4cb3c02afad80febc512",
+    "url": "./static/js/2.87945ed2.chunk.js"
   },
   {
     "revision": "8b9463c504420a0ce620e94063f0c2ae",
-    "url": "./static/js/2.123e983e.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.87945ed2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6042f032b079f2b5fd89",
-    "url": "./static/js/main.d623d5ab.chunk.js"
+    "revision": "152831e26d693ab7a517",
+    "url": "./static/js/main.77ec64df.chunk.js"
   },
   {
     "revision": "73997147ddc202a2ab2a",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a6a0c375d24251e8dc41bd232e12f8e8.js"
+  "./precache-manifest.c9b6b0352f2c279d6ad6cb4bcf25786d.js"
 );
 
 self.addEventListener('message', (event) => {
