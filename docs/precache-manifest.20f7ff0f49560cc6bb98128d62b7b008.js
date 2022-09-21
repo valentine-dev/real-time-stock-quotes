833da89ac848b868e6018b27808a3c882cbc241d
valentine-dev <valentine.business@gmail.com>
2022-09-20 17:27:41 -0700

    add new files

---
+++ docs/precache-manifest.20f7ff0f49560cc6bb98128d62b7b008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6f0af6304d4894e637c4f403d01370",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7987e0fb1319d553213",
+    "url": "./static/js/2.7fa5520d.chunk.js"
+  },
+  {
+    "revision": "8b9463c504420a0ce620e94063f0c2ae",
+    "url": "./static/js/2.7fa5520d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225a7f44a4e1c15b0e70",
+    "url": "./static/js/main.b8b671eb.chunk.js"
+  },
+  {
+    "revision": "73997147ddc202a2ab2a",
+    "url": "./static/js/runtime-main.5885bc3a.js"
+  }
+]);
