c183965bee87c8aa6372aed27ab1830ae8f06e93
valentine-dev <valentine.business@gmail.com>
2022-10-02 03:19:56 -0700

    Set ResponsiveContainer width=100% for AreaChart

---
+++ docs/precache-manifest.d667c7ab5e676ddc440ae75eacf7a2f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215ac339f20560689ef5fe432f583f16",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f62784cf3dd10c94db9",
+    "url": "./static/css/main.efcca004.chunk.css"
+  },
+  {
+    "revision": "6e461ee3cc6b6466313a",
+    "url": "./static/js/2.0a6068bc.chunk.js"
+  },
+  {
+    "revision": "f154ab84bccc9245b7d1e9896bbdcacf",
+    "url": "./static/js/2.0a6068bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f62784cf3dd10c94db9",
+    "url": "./static/js/main.0a4c1951.chunk.js"
+  },
+  {
+    "revision": "73997147ddc202a2ab2a",
+    "url": "./static/js/runtime-main.5885bc3a.js"
+  }
+]);
