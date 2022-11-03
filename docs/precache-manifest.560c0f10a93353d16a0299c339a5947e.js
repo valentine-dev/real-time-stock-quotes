0fff9cf78a68bc9a36f70a324359eb723ed019a1
valentine-dev <valentine.business@gmail.com>
2022-11-03 00:14:44 -0700

    upload missed files

---
+++ docs/precache-manifest.560c0f10a93353d16a0299c339a5947e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dcb3cdd6930fa4fa27c9219610a62e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c745e3974f139916d72",
+    "url": "./static/css/main.ad1dbf06.chunk.css"
+  },
+  {
+    "revision": "b41535c7958b492864ca",
+    "url": "./static/js/2.94a84f17.chunk.js"
+  },
+  {
+    "revision": "f154ab84bccc9245b7d1e9896bbdcacf",
+    "url": "./static/js/2.94a84f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c745e3974f139916d72",
+    "url": "./static/js/main.3c668623.chunk.js"
+  },
+  {
+    "revision": "73997147ddc202a2ab2a",
+    "url": "./static/js/runtime-main.5885bc3a.js"
+  }
+]);
